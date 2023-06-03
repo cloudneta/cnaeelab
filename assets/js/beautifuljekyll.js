d2de345ea804ae14f45511bc14e349adb56af186
cloudneta <66163453+cloudneta@users.noreply.github.com>
2023-06-04 06:13:35 +0900

    Update beautifuljekyll.js

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -137,6 +137,13 @@ var BeautifulJekyllJS = {
   }
 };
 
+$('a[href=#top]').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
 $(window).scroll(function () {
     if ($(this).scrollTop() > 50) {
         $('.totop a').fadeIn();
